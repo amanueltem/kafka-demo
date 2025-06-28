3bec132b550626beeefd9bc462dee6636a66c2db
amanueltem <amanueltemesgen62@gmail.com>
2025-06-28 17:15:08 +0300

    three applications added to the repository

--- kafka-demo/src/main/java/com/amman/kafka_demo/payload/Student.java
+++ kafka-demo/src/main/java/com/amman/kafka_demo/payload/Student.java
@@ -3,7 +3,7 @@ package com.amman.kafka_demo.payload;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
+//https://stream.wikimedia.org/v2/stream/recentChange
 @Getter
 @Setter
 @ToString
